fix: cramer's rule wrong axis replacement

linear_algebra/algebra/cramer.py
@@ -4,14 +4,14 @@ from .util import det
 
 
 def cramer(mat: np.ndarray, aug: np.ndarray) -> Iterator[np.floating]:
-    numerator = det(mat)
+    numerator = det(mat.copy())
 
     if numerator is None:
         raise ValueError("The matrix has no determinant")
 
     for i in range(mat.shape[0]):
         augmented = mat.copy()
-        augmented[i, :] = aug
+        augmented[:, i] = aug
 
         if x := det(augmented):
             yield x/numerator